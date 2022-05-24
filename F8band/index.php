7d7ec9d0f0c56bf41a7a946664dca1e73de65b5a
Linh1605 <hoanglinh16052001@gmail.com>
2022-05-24 18:23:49 +0700

    first commit

---
+++ F8band/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+     <link rel="stylesheet" href="./fonts/themify-icons/themify-icons.css"> 
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+     <!-- <link rel="stylesheet" href="<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />  -->
+    <title>Document</title>
+</head>
+<body>
+<div class="main">
+        <div class="header">
+            <ul class="nav">
+                <li><a href="">Home</a></li>
+                <li><a href="">Band</a></li>
+                <li><a href="">Tour</a></li>
+                <li><a href="">Contact</a></li>
+                <li>
+                    <a href="">
+                        More
+                        <!-- <i class="nav-arrow-down ti-angle-down"></i>  -->
+                        <i class="nav-arrow-down fas fa-sort-down"></i>
+                    </a>
+                    <ul class="subnav">
+                        <li><a href="">Merchandise</a></li>
+                        <li><a href="">Extras</a></li>
+                        <li><a href="">Media</a></li>
+                    </ul>
+                </li>
+
+            </ul>
+            <div class="search-btn">
+                <i class="search-icon ti-search"></i>
+            </div>
+
+        </div>
+        
+        <div class="slider">
+            <div class="text-content">
+              <h2 class="text-heading">Chicago</h2>
+                <div class="text-decription">Thank you, Chicago - A night we won't forget.</div>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="content-section">
+                <h2 class="section-heading">THE BAND</h2>
+                <p class="section-sub-heading">We love music</p>
+                <p class="about-text">We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                <div class="member-list">
+                    <div class="member-item">
+                        <p class="member-name">Name</p>
+                        <img src="./img/content/bandmember.jpg" alt="Name" class="member-img">
+                    </div>
+                    <div class="member-item">
+                        <p class="member-name">Name</p>
+                        <img src="./img/content/bandmember.jpg" alt="Name" class="member-img">
+                    </div>
+                    <div class="member-item">
+                        <p class="member-name">Name</p>
+                        <img src="./img/content/bandmember.jpg" alt="Name" class="member-img">
+                    </div>
+                    <div class="clear"></div>
+                </div>
+                
+            </div>
+            <div class="tour-section">
+
+                <div class="content-section">
+                    <h2 class="section-heading text-white">TOUR DATES</h2>
+                    <p class="section-sub-heading text-white">Remember to book your tickets!</p>
+                    <ul class="tickets-list">
+                        <li>September <span class="sold-out">Sold-out</span></li>
+                        <li>October <span class="sold-out">Sold-out</span></li>
+                        <li>November <span class="quantity">3</span></li>
+                    </ul>
+                    <div class="place-list">
+                    <div class="place-item">
+                        <img src="./img/tour_date/newyork.jpg" alt="New York" class="place-img">
+                        <div class="place-body">
+                            <h3 class="place-heading">New York</h3>
+                            <p class="place-time">Fri 27 Nov 2016</p>
+                            <p class="desc">Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+                            <a href="#" class="place-buy-btn">Buy Tickets</a>
+                        </div>
+                    </div>
+                    <div class="place-item">
+                        <img src="./img/tour_date/paris.jpg" alt="New York" class="place-img">
+                        <div class="place-body">
+                            <h3 class="place-heading">Paris</h3>
+                            <p class="place-time">Sat 28 Nov 2016</p>
+                            <p class="desc">Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+                            <a href="#" class="place-buy-btn">Buy Tickets</a>
+                        </div>
+                    </div>
+                    <div class="place-item">
+                        <img src="./img/tour_date/sanfran.jpg" alt="New York" class="place-img">
+                        <div class="place-body">
+                            <h3 class="place-heading">San Francisco</h3>
+                            <p class="place-time">Sun 29 Nov 2016</p>
+                            <p class="desc">Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+                            <a href="#" class="place-buy-btn">Buy Tickets</a>
+                        </div>
+                    </div>
+                    <div class="clear"></div>
+                </div>
+            </div>
+            </div>
+            
+            <div class="content-section">
+                <h2 class="section-heading">CONTACTs</h2>
+                <p class="section-sub-heading">Fan? Drop a note!</p>
+                <div class="contact">
+                    <div class="contact-content">
+                <div class="contact-section">
+                       <i class="ti-location-pin"></i>
+                       <p class="contact-heading-section">Chicago, US</p>
+                    </div>
+                    <div class="contact-section">
+                       <i class="ti-location-pin"></i>
+                       <p class="contact-heading-section">Chicago, US</p>
+                    </div>
+                    <div class="contact-section">
+                       <i class="ti-location-pin"></i>
+                       <p class="contact-heading-section">Chicago, US</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="footer">
+            <div class="footer-header">
+                
+            </div>
+
+        </div>
+
+        
+    </div>
+</body>
+</html>
